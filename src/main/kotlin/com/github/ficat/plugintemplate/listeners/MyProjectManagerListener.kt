bc110869e27c87ee7302523b0ba7b87e1817288d
GitHub Action <action@github.com>
2021-09-07 03:22:36 +0000

    Template cleanup

--- src/main/kotlin/com/github/ficat/plugintemplate/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/ficat/plugintemplate/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.ficat.plugintemplate.listeners
 
+import com.github.ficat.plugintemplate.services.MyProjectService
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/ficat/plugintemplate/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/ficat/plugintemplate/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.ficat.plugintemplate.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.ficat.plugintemplate.MyBundle
 
 class MyApplicationService {
 
